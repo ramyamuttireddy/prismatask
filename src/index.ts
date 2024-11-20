f6ee56cd02af74872e16d0091400151a6194dd1c
Ramya <muttireddyramya9@gmail.com>
2024-11-20 12:27:41 +0530

    chore-intial commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { Elysia } from "elysia";
+import productRouter from "./routes/productRouter";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+
+app.get("/",(req) =>"Hello Elysia");
+app.use(productRouter()).listen(3000);
+
+
+
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
---
+++ src/routes/productRouter.ts
@@ -0,0 +1,22 @@
+import Elysia from "elysia";
+
+const productRouter = new Elysia().group("/products",(app) => 
+    app.get("/list",() => {
+        return[];
+    })
+    .get("/:id",(req)=> {
+        return req.params.id
+    })
+    .post("/",(req) =>{
+         return req.body
+    })
+
+    .put("/:id",(req) => {
+        return req.body
+    })
+    .delete("/:id" ,(req) => {
+        return req.params.id;
+    })
+)
+
+export default productRouter
